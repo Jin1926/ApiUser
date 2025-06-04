7d00633c6f8de7055e2f8f499689ed8692c83a5b
Jin1926 <jinfer42@gmail.com>
2025-06-04 17:37:21 -0500

    Concectando

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
-// db.js
 require('dotenv').config(); // Importa dotenv al inicio
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+module.exports = pool;
+
 
 module.exports = pool;
