31a7b82fbef2449d401048ae740e9e7f6ba1325f
Jin1926 <jinfer42@gmail.com>
2025-06-04 21:56:42 -0500

    fix

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_PUBLIC_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
