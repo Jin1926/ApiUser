24e195ad12b9d11e17c7ce56dd7194eb9d124fca
Jin1926 <jinfer42@gmail.com>
2025-06-04 22:15:13 -0500

    fix

--- index.js
+++ index.js
@@ -105,6 +105,7 @@ app.post('/api/solicitar-comic', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
