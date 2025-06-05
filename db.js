13db15805610fc679d454312a2dba07f9384daac
Jin1926 <jinfer42@gmail.com>
2025-06-04 21:36:33 -0500

    fix

--- db.js
+++ db.js
@@ -13,13 +13,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
+    connectionString: process.env.DATABASE_PUBLIC_URL,
     ssl: {
         rejectUnauthorized: false
     }
 });
-
-module.exports = pool;
-
 
 module.exports = pool;
